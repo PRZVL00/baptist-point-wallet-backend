41ec41c49796b7426736ca6e318eec74d8d56931
SheikBautista <85090090+PRZVL00@users.noreply.github.com>
2025-10-17 12:35:12 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # For local development
-    # Add your Render frontend URL later
+    "https://baptist-point-wallet-frontend.onrender.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
